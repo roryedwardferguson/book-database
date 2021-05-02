1b9525798aabf835b4d9505cd083d79c49c17158
Rory Ferguson <roryferguson@Rorys-MacBook-Pro.local>
2021-05-02 23:36:41 +0100

    Ability to search for books

--- app.py
+++ app.py
@@ -69,6 +69,31 @@ def clean_price(price_str):
     else: 
         return int(price_float * 100)
 
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+
+    except ValueError:
+        input('''
+                \n**** ID ERROR *****
+                \r The id should be a number
+                \r*********************
+                \r Press enter to try again
+              ''')
+        return 
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f'''
+                \n**** OPTIONS ERROR *****
+                \r Options: {options}
+                \r*********************
+                \r Press enter to try again
+              ''')
+            return    
+
+
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -116,7 +141,23 @@ def app():
             input('\nPress enter to return to the main menu.')    
         elif choice == '3':
             # search book
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:    
+                id_choice = input(f'''
+                    \nId Options:{id_options}
+                    \rBook id:''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id == id_choice).first()
+            print(f'''
+                  \n{the_book.title} by {the_book.author}
+                  \rPublished: {the_book.published_date}
+                  \rPrice: ${the_book.price /  100 }''')
+            input('Press enter to return to the main menu')              
         elif choice == '4':
             # book analysis
             pass
